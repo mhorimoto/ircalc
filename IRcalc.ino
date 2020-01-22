b56705b490766b8c3678c811eaa21e531cd76495
Masafumim Horimoto on smena8 <masafumi.horimoto@gmail.com>
2020-01-23 01:20:04 +0900

    Rtcの出力まで確認

--- IRcalc.ino
+++ IRcalc.ino
@@ -1,5 +1,7 @@
 #include <IRremote.h>
 #include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+#include <RtcDS3231.h>
 
 #define POWER 0x2FD48B7
 #define HOME  0x27D2CD3
@@ -47,12 +49,15 @@ int RECV_PIN = 6;
 
 IRrecv irrecv(RECV_PIN);
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+RtcDS3231<TwoWire> Rtc(Wire);
 
 decode_results results;
 
 int lcdx,lcdy,x;
 int stack[MAX_STACK_LEVEL];
 bool enpr = false;
+bool bklight = true;
+bool inMenu = false;
 
 void setup() {
   int sp;
@@ -64,58 +69,126 @@ void setup() {
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0,0);
-  lcd.print("CALC V1.00");
-  Serial.println("CALC V1.00");
+  lcd.print("CALC V1.38");
+  Serial.println("CALC V1.38");
+  lcdx=0;lcdy=0;
+  Rtc.Begin();
 }
 
 void loop() {
   int sp,d,i,j,k;
   char c;
   char textbuf[256];
+  char lcdbuf[17];
   if (irrecv.decode(&results)) {
-    switch(results.value) {
-    case NUM0: d = 0; break;
-    case NUM1: d = 1; break;
-    case NUM2: d = 2; break;
-    case NUM3: d = 3; break;
-    case NUM4: d = 4; break;
-    case NUM5: d = 5; break;
-    case NUM6: d = 6; break;
-    case NUM7: d = 7; break;
-    case NUM8: d = 8; break;
-    case NUM9: d = 9; break;
-    case ENTER:
-      stack_shift(-1,MAX_STACK_LEVEL,true);
-      break;
-    case PLUS:
-      x += stack[0];
-      Serial.println(); Serial.println(x);
-      stack_shift(1,MAX_STACK_LEVEL,true);
-      break;
-    case MINUS:
-      stack[0] -= x;
-      x = stack[0];
-      Serial.println(); Serial.println(x);
-      stack_shift(1,MAX_STACK_LEVEL,true);
-      break;
-    case MULTI:
-      x *= stack[0];
-      Serial.println(); Serial.println(x);
-      stack_shift(1,MAX_STACK_LEVEL,true);
-      break;
-    case DIV:
-      x /= stack[0];
-      Serial.println(); Serial.println(x);
-      stack_shift(1,MAX_STACK_LEVEL,true);
-      break;
-    default:
-      d = -1;
-      break;
-    }
-    if ((d>=0)&&(d<=9)) {
-      x *= 10;
-      x += d;
-      Serial.print(d,DEC);
+    if (inMenu) {
+      switch(results.value) {
+      case POWER:
+	if (bklight) {
+	  bklight = false;
+	  lcd.noBacklight();
+	} else {
+	  bklight = true;
+	  lcd.backlight();
+	}
+	break;
+      case MUTE:
+	inMenu = false;
+	break;
+      case PRG:
+	RtcDateTime now = Rtc.GetDateTime();
+	printDateTime(now);
+	Serial.println();
+	lcd.setCursor(0,0);
+	lcd.print("CLOCK");
+	lcd.setCursor(0,1);
+	lcd.print(now);
+	break;
+      default:
+	if (results.value!=0xffffffff) {
+	  lcd.setCursor(0,1);
+	  sprintf(lcdbuf,"0x%02X%02X%02X%02X",(byte)((results.value>>24)&(0xff)),
+		  (byte)((results.value>>16)&(0xff)),
+		  (byte)((results.value>>8)&(0xff)),(byte)results.value&(0xff));
+	  Serial.println(lcdbuf);
+	  break;
+	}
+      }
+    } else {
+      switch(results.value) {
+      case MUTE:
+	inMenu = true;
+	lcd.setCursor(0,0);
+	lcd.print("MENU MODE       ");
+	break;
+      case POWER:
+	if (bklight) {
+	  bklight = false;
+	  lcd.noBacklight();
+	} else {
+	  bklight = true;
+	  lcd.backlight();
+	}
+	break;
+      case NUM0: d = 0; break;
+      case NUM1: d = 1; break;
+      case NUM2: d = 2; break;
+      case NUM3: d = 3; break;
+      case NUM4: d = 4; break;
+      case NUM5: d = 5; break;
+      case NUM6: d = 6; break;
+      case NUM7: d = 7; break;
+      case NUM8: d = 8; break;
+      case NUM9: d = 9; break;
+      case ENTER:
+	lcdx = 0;
+	lcd.setCursor(0,1);
+	lcd.print("                ");
+	lcd.setCursor(0,1);
+	lcd.print(0,DEC);
+	stack_shift(-1,MAX_STACK_LEVEL,true);
+	break;
+      case PLUS:
+	lcdx = 0;
+	x += stack[0];
+	lcd.setCursor(0,1);
+	lcd.print("                ");
+	lcd.setCursor(0,1);
+	lcd.print(x,DEC);
+	Serial.println(); Serial.println(x);
+	stack_shift(1,MAX_STACK_LEVEL,true);
+	break;
+      case MINUS:
+	lcdx = 0;
+	stack[0] -= x;
+	x = stack[0];
+	Serial.println(); Serial.println(x);
+	stack_shift(1,MAX_STACK_LEVEL,true);
+	break;
+      case MULTI:
+	lcdx = 0;
+	x *= stack[0];
+	Serial.println(); Serial.println(x);
+	stack_shift(1,MAX_STACK_LEVEL,true);
+	break;
+      case DIV:
+	lcdx = 0;
+	x /= stack[0];
+	Serial.println(); Serial.println(x);
+	stack_shift(1,MAX_STACK_LEVEL,true);
+	break;
+      default:
+	d = -1;
+	break;
+      }
+      if ((d>=0)&&(d<=9)) {
+	x *= 10;
+	x += d;
+	Serial.print(d,DEC);
+	lcd.setCursor(lcdx,1);
+	lcd.print(d);
+	lcdx++;
+      }
     }
     irrecv.resume(); // Receive the next value
   }
@@ -129,6 +202,12 @@ void stack_shift(int direction,int max_stack,bool enpr) {
     if (enpr) {
       for(j=0;j<max_stack;j++) {
 	sprintf(textbuf,"SP[%d]=%d",j,stack[j]);
+	if (j==0) {
+	  lcd.setCursor(0,0);
+	  lcd.print("                ");
+	  lcd.setCursor(0,0);
+	  lcd.print(textbuf);
+	}
 	Serial.println(textbuf);
       }
     }
@@ -140,8 +219,26 @@ void stack_shift(int direction,int max_stack,bool enpr) {
       Serial.println();
       for(j=0;j<max_stack;j++) {
 	sprintf(textbuf,"SP[%d]=%d",j,stack[j]);
+	if (j==0) {
+	  lcd.setCursor(0,0);
+	  lcd.print("                ");
+	  lcd.setCursor(0,0);
+	  lcd.print(textbuf);
+	}
 	Serial.println(textbuf);
       }
     }
   }
+}
+void printDateTime(const RtcDateTime& dt) {
+  char datestring[20];
+
+  sprintf(datestring, "%02u/%02u/%04u %02u:%02u:%02u",
+	     dt.Month(),
+	     dt.Day(),
+	     dt.Year(),
+	     dt.Hour(),
+	     dt.Minute(),
+	     dt.Second() );
+  Serial.print(datestring);
 }
